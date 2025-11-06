1e06b0d1deae25ed56d28278f01ed568c48bd4af
kernelcoin <kernelcoin@llify.com>
2025-11-06 01:57:11 -0600

    fixing 0

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -6,7 +6,7 @@
  *
  * Each line contains a BIP155 serialized (networkID, addr, port) tuple.
  */
-static const uint8_t chainparams_seed_main[] = {};
+static const uint8_t chainparams_seed_main[] = {0};
 
-static const uint8_t chainparams_seed_test[] = {};
+static const uint8_t chainparams_seed_test[] = {0};
 #endif // BITCOIN_CHAINPARAMSSEEDS_H
